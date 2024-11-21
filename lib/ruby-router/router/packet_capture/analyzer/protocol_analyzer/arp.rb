a1de2d9335442ffae0ad2a9b176c135a51c896cd
618knot <publicac.clt618knot@gmail.com>
2024-11-21 16:07:06 +0900

    インスタンス変数管理に変更

--- lib/ruby-router/router/packet_capture/analyzer/protocol_analyzer/arp.rb
+++ lib/ruby-router/router/packet_capture/analyzer/protocol_analyzer/arp.rb
@@ -29,55 +29,63 @@ module ProtocolAnalyzer
       nil,
       nil,
       "Metricom STRIP (new IANA id)."
-  ].freeze
+    ].freeze
 
-  OP = [
-    nil,
-    "ARP request.",
-    "ARP reply.",
-    "RARP request.",
-    "RARP reply.",
-    nil,
-    nil,
-    nil,
-    "InARP request.",
-    "InARP reply.",
-    "(ATM)ARP NAK."
-].freeze
+    OP = [
+      nil,
+      "ARP request.",
+      "ARP reply.",
+      "RARP request.",
+      "RARP reply.",
+      nil,
+      nil,
+      nil,
+      "InARP request.",
+      "InARP reply.",
+      "(ATM)ARP NAK."
+    ].freeze
+
+    attr_reader(
+      :arp_hrd,
+      :arp_pro,
+      :arp_hln,
+      :arp_pln,
+      :arp_op,
+      :arp_sha,
+      :arp_spa,
+      :arp_tha,
+      :arp_tpa
+      )
 
     def analyze
-      arp = {}
+      @arp_hrd = @msg_bytes.slice(0..1)    # Hardware Type:      2Byte
+      @arp_pro = @msg_bytes.slice(2..3)    # Protocol Type:      2Byte
+      @arp_hln = @msg_bytes.slice(4)       # Hardware Size:      1Byte
+      @arp_pln = @msg_bytes.slice(5)       # Protocol Size:      1Byte
+      @arp_op = @msg_bytes.slice(6..7)     # Opcode:             2Byte
+      @arp_sha = @msg_bytes.slice(8..13)   # Sender MAC address: 6Byte
+      @arp_spa = @msg_bytes.slice(14..17)  # Sender IP address:  4Byte
+      @arp_tha = @msg_bytes.slice(18..23)  # Target MAC address: 6Byte
+      @arp_tpa = @msg_bytes.slice(24..27)  # Target IP address:  4Byte
 
-      arp[:arp_hrd] = @msg_bytes.slice(0..1)    # Hardware Type:      2Byte
-      arp[:arp_pro] = @msg_bytes.slice(2..3)    # Protocol Type:      2Byte
-      arp[:arp_hln] = @msg_bytes.slice(4)       # Hardware Size:      1Byte
-      arp[:arp_pln] = @msg_bytes.slice(5)       # Protocol Size:      1Byte
-      arp[:arp_op] = @msg_bytes.slice(6..7)     # Opcode:             2Byte
-      arp[:arp_sha] = @msg_bytes.slice(8..13)   # Sender MAC address: 6Byte
-      arp[:arp_spa] = @msg_bytes.slice(14..17)  # Sender IP address:  4Byte
-      arp[:arp_tha] = @msg_bytes.slice(18..23)  # Target MAC address: 6Byte
-      arp[:arp_tpa] = @msg_bytes.slice(24..27)  # Target IP address:  4Byte
-
-      print_arp(arp)
-
-      arp
+      print_arp
     end
 
     private
 
-    def print_arp(arp)
+    def print_arp
       @logger.info("■■■■■ ARP ■■■■■")
 
       msg = [
-        "Hardware Type => #{HRD[self.to_hex_int(arp[:arp_hrd])]}",
-        "Protocol Type => #{Constants::EtherTypes::STR_HASH[self.to_hex_int(arp[:arp_pro])]}",
-        "Hardware Size => #{arp[:arp_hln]} Byte",
-        "Protocol Size => #{arp[:arp_pln]} Byte",
-        "Opcode => #{OP[self.to_hex_int(arp[:arp_op])]}",
-        "Sender MAC address => #{macaddr_to_s(arp[:arp_sha])}",
-        "Sender IP address => #{arp[:arp_spa].join(".")}",
-        "Target MAC address => #{macaddr_to_s(arp[:arp_tha])}",
-        "Target IP address => #{arp[:arp_tpa].join(".")}"
+        "Hardware Type => #{HRD[self.to_hex_int(@arp_hrd)]}",
+        "Protocol Type => #{Constants::EtherTypes::STR_HASH[self.to_hex_int(@arp_pro)]}",
+        "Hardware Size => #{@arp_hln} Byte",
+        "Protocol Size => #{@arp_pln} Byte",
+        "Opcode => #{OP[self.to_hex_int(@arp_op)]}",
+        "Sender MAC address => #{macaddr_to_s(@arp_sha)}",
+        "Sender IP address => #{@arp_spa.join(".")}",
+        "Target MAC address => #{macaddr_to_s(@arp_tha)}",
+        "Target IP address => #{@arp_tpa.join(".")}"
       ]
 
       msg.map { |m| @logger.debug(m) }
