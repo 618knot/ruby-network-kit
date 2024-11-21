0ebd0999862a34a973ea8ab7feeb6bb65ccf8d57
618knot <publicac.clt618knot@gmail.com>
2024-11-21 14:49:50 +0900

    etherheader修正

--- lib/ruby-router/router/packet_capture/analyzer/header/ether_header.rb
+++ lib/ruby-router/router/packet_capture/analyzer/header/ether_header.rb
@@ -5,19 +5,10 @@
 #       ref: https://shinmeisha.co.jp/newsroom/2020/06/09/%E3%82%A4%E3%83%BC%E3%82%B5%E3%83%8D%E3%83%83%E3%83%88%E3%83%98%E3%83%83%E3%83%80%E3%81%AE%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88%E3%81%A8%E3%82%B5%E3%82%A4%E3%82%BA%E3%81%AE%E5%9F%BA/
 
 require_relative "../../../constants"
-
-# module PacketCapture
-include Constants
 class EtherHeader
-  ETHERTYPE = {
-    Constants::EtherTypes::PUP => "Xerox PUP",
-    Constants::EtherTypes::IP => "IPv4",
-    Constants::EtherTypes::ARP => "Address resolution",
-    Constants::EtherTypes::REVARP => "Reverse ARP",
-    Constants::EtherTypes::IPv6 => "IPv6",
-  }
+  include Constants
 
-  attr_reader :dst_mac_address, :src_mac_address, :type
+  attr_reader :dst_mac_address, :src_mac_address, :type, :int_hex_type
 
   #
   # @param [Array] msg_bytes
@@ -29,12 +20,12 @@ class EtherHeader
   #
   # ヘッダー情報を出力する
   #
-  def print_headers
+  def print_header
     logger = CustomLogger.new
     logger.info("■■■■■ Ether Header ■■■■■")
     logger.debug("dst_mac_address=> #{macaddr_to_s(dst_mac_address)}")
     logger.debug("src_mac_address=> #{macaddr_to_s(src_mac_address)}")
-    logger.debug("type=> #{type_to_s(type)}")
+    logger.debug("type=> #{type_to_s}")
   end
 
   private
@@ -53,6 +44,7 @@ class EtherHeader
     @dst_mac_address = msg_bytes.slice(0..5)
     @src_mac_address = msg_bytes.slice(6..11)
     @type = msg_bytes.slice(12..13)
+    hex_type(@type)
   end
 
   #
@@ -68,16 +60,23 @@ class EtherHeader
   end
 
   #
-  # ETHERTYPE文字列にする
+  # hexのEthertype値
   #
   # @param [Array] type typeのbyte array
   #
+  # @return [Integer] Ethertypeのhex値
+  #
+  def hex_type(type)
+    hex_type = type.map { |byte| byte.to_s(16).rjust(2, "0") }
+    @int_hex_type = hex_type.join.to_i(16)
+  end
+
+  #
+  # ETHERTYPE文字列にする
+  #
   # @return [String or nil] ETHERTYPE文字列(該当するものがないときはnil)
   #
-  def type_to_s(type)
-    hex_type = type.map { |byte| byte.to_s(16).rjust(2, "0") }
-    int_hex_type = hex_type.join.to_i(16)
-
-    ETHERTYPE[int_hex_type]
+  def type_to_s
+    Constants::EtherTypes::STR_HASH[self.int_hex_type]
   end
 end
