684cebc3cfcba7bccaa267027fdf40ffcee7e823
618knot <publicac.clt618knot@gmail.com>
2024-11-21 14:49:26 +0900

    EtherTypesにhashを追加

--- lib/ruby-router/router/constants.rb
+++ lib/ruby-router/router/constants.rb
@@ -6,5 +6,13 @@ module Constants
     ARP = 0x0806.freeze # Address resolution
     REVARP = 0x8035.freeze # Reverse ARP
     IPv6 = 0x86dd.freeze # IPv6
+
+    STR_HASH = {
+      PUP => "Xerox PUP",
+      IP => "IPv4",
+      ARP => "Address resolution",
+      REVARP => "Reverse ARP",
+      IPv6 => "IPv6",
+  }.freeze
   end
 end
