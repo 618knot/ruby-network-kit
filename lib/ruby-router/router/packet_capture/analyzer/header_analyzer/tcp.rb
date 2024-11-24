b29cec0959bebd6df54d8bc9ec7ca5d2da093205
618knot <publicac.clt618knot@gmail.com>
2024-11-24 21:38:54 +0900

    attr_reader忘れ

--- lib/ruby-router/router/packet_capture/analyzer/header_analyzer/tcp.rb
+++ lib/ruby-router/router/packet_capture/analyzer/header_analyzer/tcp.rb
@@ -2,11 +2,22 @@
 
 module HeaderAnalyzer
   class Tcp < Header
+    attr_reader(
+      :source,
+      :dest,
+      :seq,
+      :ack_seq,
+      :doff,
+      :flags,
+      :window,
+      :check,
+      :urg_ptr
+    )
     def analyze
       @source = @msg_bytes.slice(0..1)    # Source Port:       2Byte
       @dest = @msg_bytes.slice(2..3)      # Destination Port:  2Byte
       @seq = @msg_bytes.slice(4..7)       # Sequence Number:   4Byte
-      @ack_swq = @msg_bytes.slice(8..11)  # ACK Number:        4Byte
+      @ack_seq = @msg_bytes.slice(8..11)  # ACK Number:        4Byte
       @doff = @msg_bytes.slice(12)[4..7]  # Data Offset:       4bit
       @flags = @msg_bytes.slice(13)[0..5] # Flags:             6bit
       @window = @msg_bytes.slice(14..15)  # Window Size:       2Byte
@@ -16,7 +27,7 @@ module HeaderAnalyzer
       @source = self.to_hex_int(@source)
       @dest = self.to_hex_int(@dest)
       @seq = self.to_hex_string(@seq, is_formated: true)
-      @ack_swq = self.to_hex_string(@ack_swq, is_formated: true)
+      @ack_seq = self.to_hex_string(@ack_seq, is_formated: true)
       @window = self.to_hex_int(@window)
       @check = self.to_hex_string(@check, is_formated: true)
       @urg_ptr = self.to_hex_int(@urg_ptr)
@@ -41,7 +52,7 @@ module HeaderAnalyzer
         "Source Port => #{@source}",
         "Destination Port => #{@dest}",
         "Sequence Number => #{@seq}",
-        "ACK Number => #{@ack_swq}",
+        "ACK Number => #{@ack_seq}",
         "Data Offset => #{@doff} (#{@doff * 4} Byte)",
         "Flags => #{flags_to_array(@flags).join(", ")}",
         "WIndow Size => #{@window} Byte",
---
+++ lib/ruby-router/router/packet_capture/analyzer/header_analyzer/udp.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module HeaderAnalyzer
+  class Udp < Header
+    
+  end
+end
