2580aa15b36def2ce63bb987eaeb414413ff309c
618knot <publicac.clt618knot@gmail.com>
2024-12-09 17:36:32 +0900

    router -> kit

--- lib/ruby-router/ruby-router.rb
+++ lib/ruby-router/ruby-router.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "router/custon_logger"
-require_relative "router/string"
-require_relative "router/packet_capture/packet_capture"
-require_relative "router/bridge/bridge"
+require_relative "kit/custon_logger"
+require_relative "kit/string"
+require_relative "kit/packet_capture/packet_capture"
+require_relative "kit/bridge/bridge"
 
 module RubyRouter
   include PacketCapture
