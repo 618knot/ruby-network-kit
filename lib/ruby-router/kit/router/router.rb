3cc5d17104f09b07cfbc1f1aa8195086c5412883
618knot <publicac.clt618knot@gmail.com>
2024-12-20 15:47:49 +0900

    メイン処理暫定実装

--- lib/ruby-router/kit/router/router.rb
+++ lib/ruby-router/kit/router/router.rb
@@ -4,6 +4,9 @@ require_relative "../socket_utils"
 require_relative "net_util"
 require_relative "../packet_analyzer/packet_analyzer"
 require_relative "router_base"
+require_relative "../packet_analyzer/packet_analyzer"
+require_relative "../constants"
+require "ipaddr"
 
 module Router
   class Router < RouterBase
@@ -36,13 +39,14 @@ module Router
 
           next unless readable
 
-          readable.each do |socket| 
+          readable.each do |socket|
             begin
               data = socket.read_nonblock(buf_size)
               if data.nil? || data.empty?
                 @logger.debug("Connection closed or no data")
               else
-                # analyze_packet(device, data)
+                d_idx = search_device_idx(socket)
+                analyze_packet(d_idx, data)
               end
             rescue IO::WaitReadable
               next
@@ -56,6 +60,8 @@ module Router
         end
       end
     end
+
+    private
 
     def send_icmp_time_exceeded(device_no, ether_header, ip_header, data)
       device = self.devices[device_no]
@@ -76,7 +82,7 @@ module Router
         ttl: 64,
         protocol: 1,
         check: 0,
-        saddr: device.addr,
+        saddr: ip_addr_to_arr(device.addr),
         daddr: ip_header.saddr,
       )
 
@@ -97,15 +103,27 @@ module Router
       socket.write(packet)
     end
 
-    def test
-      source_mac = [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]   # 送信元MAC
-      target_mac = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]   # ブロードキャストMAC
-      source_ip  = [192, 168, 1, 10]                      # 送信元IP
-      target_ip  = [192, 168, 1, 20]                      # ターゲットIP
-      send_arp_request(init_socket(@interface1), source_mac, target_mac, source_ip, target_ip)
-    end
+    def analyze_packet(device_no, data)
+      @analyzed_data = PacketAnalyzer.new(data.bytes, disable_log: true).analyze
+      ether = @analyzed_data[:ether]
 
-    private
+      if ether.nil?
+        @logger.debug("#{@devices[device_no].if_name}: Ethernet header not found")
+        return
+      end
+
+      if ether.dst_mac_address != @devices[device_no].hwaddr
+        @logger.debug("#{@devices[device_no].if_name}: Destination MAC does not match => #{ether.dst_mac_address}")
+        return
+      end
+
+      case ether.type
+      when Constants::EtherTypes::ARP
+        analyze_arp(device_no, @analyzed_data[:arp])
+      when Constants::EtherTypes::IP
+        analyze_ip(device_no, @analyzed_data[:ip], ether, data)
+      end
+    end
 
     def disable_ip_forward
       File.open("/proc/sys/net/ipv4/ip_forward", "w") do |f|
@@ -117,6 +135,94 @@ module Router
       File.open("/proc/sys/net/ipv4/ip_forward", "w") do |f|
         f.puts "1"
       end
+    end
+
+    def search_device_idx(socket)
+      @devices.each_with_index do |d, i|
+        return i if d.socket == socket
+      end
+    end
+
+    def analyze_arp(device_no, arp)
+      arp_op = to_hex_int(arp.arp_op)
+
+      @logger.debug("#{@devices[device_no].if_name}: Receive ARP REQUEST") if arp_op == 1 # ARPOP_REQUEST
+      @logger.debug("#{@devices[device_no].if_name}: Receive ARP REPLY") if arp_op == 2 # ARPOP_REPLY
+      
+      ip2mac(device_no, arp.arp_spa, arp.arp_sha)
+    end
+
+    def analyze_ip(device_no, ip, ether, data)
+      ip_cpy = IP.new
+      ip_cpy.copy_from_analyzed(ip)
+
+      ip_checksum = checksum(ip_cpy.to_binary.bytes)
+
+      unless valid_checksum?(ip_checksum)
+        @logger.debug("#{@devices[device_no].if_name}: Bad IP checksum")
+        return
+      end
+
+      if ip_cpy.ttl <= 1
+        @logger.debug("#{@devices[device_no].if_name}: TTL expired")
+        send_icmp_time_exceeded(device_no, ether, ip_cpy, data)
+        return
+      end
+
+      dest_ip = ip_cpy.dest_ip.join(":")
+      sender_devices = @devices.clone.delete_at(device_no)
+
+      sender_devices.each_with_index do |device, idx|
+        if (IPAddr.new(dest_ip) & IPAddr.new(device.netmask).to_i) == IPAddr.new(devive.subnet).to_i
+          handle_segment(device_no, idx, ip_cpy, data)
+        else
+          handle_next(device_no, idx, ip_cpy, data)
+        end
+      end
+    end
+
+    def handle_segment(device_no, tno, ip, data)
+      dest_ip = ip.dest_ip.join(":")
+      if dest_ip == @devices[device_no].addr
+        @logger.debug("#{@devices[device_no].if_name}: Received for this device")
+
+        return
+      end
+
+      ip2mac = ip_to_mac(tno, dest_ip, nil)
+      if ip2mac.flag == :ng || ip2mac.send_data.dno != 0
+        append_send_data(ip2mac, 1, ip.dest_ip, data)
+      else
+        forward_packet(ip2mac.hwaddr, tno, ip, data)
+      end
+    end
+
+    def handle_next(device_no, tno, ip, data)
+      ip2mac = ip_to_mac(tno, @next_ip, nil)
+      if ip2mac.flag == :ng || ip2mac.send_data.dno != 0
+        append_send_data(ip2mac, 1, @next_ip, nil)
+      else
+        forward_packet(ip2mac.hwaddr, tno, ip, data)
+      end
+    end
+
+    def forward_packet(dest_mac, tno, ip, data)
+      ip.ttl -= 1
+      ip_checksum_for_sending(ip)
+
+      analyzed_eth = @analyzed_data[:ether]
+
+      ether_bin = ETHER.new(
+        dest_mac.pack("C*"),
+        analyzed_eth.dst_mac_address.pack("C*"),
+        analyzed_eth.type.pack("S>"),
+      ).to_binary
+
+      ip_bin = ip.to_binary
+
+      packet = ether_bin + ip_bin + data[ether_bin.length + ip_bin.length..]
+
+      @devices[tno].socket.write(packet)
     end
   end
 end
